30444692b13a24c26dfcd426c3d3b4a5a07d0373
Shumi <gagnaire.pauline@gmail.com>
2023-03-29 12:24:01 +0200

    Creation des projet dans la gallerie
    Script.js : Ajout des differents elements contenu dans un projet puis ajout des differents projets dans la gallery

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -18,7 +18,21 @@ async function getWorks() {
         const data = await response.json(); // Travaux sous forme d'objets manipulables
 
         for (let i in data) {
-            console.log(data);
+            const figure = document.createElement("figure");
+            const img = document.createElement("img");
+            const figcaption = document.createElement("figcaption");
+
+            figure.setAttribute("data-category-id", data[i].category.id)
+            figure.setAttribute("data-id", data[i].id)
+            img.setAttribute("src", data[i].imageUrl);
+            img.setAttribute("alt", data[i].title);
+            img.setAttribute("crossorigin", "anonymous");
+            figcaption.innerHTML = data[i].title;
+
+            figure.append(img, figcaption);
+            gallery.append(figure);
+
+            figures.push(figure); //On push chaque figure dans le tableau figures de manière à pouvoir utiliser chaque figure à l'exterieur de la boucle
         }
     }
     catch (error) {
