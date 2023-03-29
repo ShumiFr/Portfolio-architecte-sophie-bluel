fd972e380b40179444b757247e2688c9f76678fb
Shumi <gagnaire.pauline@gmail.com>
2023-03-29 09:47:33 +0200

    Ajout de la supression des prokets

--- FrontEnd/js/function.js
+++ FrontEnd/js/function.js
@@ -1,39 +1,82 @@
+//Declaration des variables
 let urlAPI = "http://localhost:5678/api/works";
-let works;
 let token =  sessionStorage.getItem("token");
+let projects;
 
-const createProjectHTML = (project) => {
-    const figure = document.createElement("figure");
-    const img = document.createElement("img");
-    const deleteButton = document.createElement("button");
-  
-    img.setAttribute("src", project.imageUrl);
-    img.setAttribute("alt", project.title);
-    img.setAttribute("cross-origin", "anonymous");
-  
-    deleteButton.innerText = "Supprimer";
-    deleteButton.addEventListener("click", () => {
-        // Ajouter ici le code pour supprimer le projet (envoi d'une requête DELETE)
-    });
-  
-    figure.appendChild(img);
-    figure.appendChild(deleteButton);
-  
-    return figure;
+//Récupération des données depuis l'API
+fetch(urlAPI, {
+    headers: {
+        "Authorization": "Bearer " + token
+    }
+})
+.then(response => response.json())
+.then(data => {
+    //Assigner les données à la variable projects
+    projects = data;
+    //Afficher les projets dans la modale
+    createProjectModal(projects);
+})
+.catch(error => console.error(error));
+
+//Fonction pour afficher et supprimer les projets dans la modale
+const createProjectModal = (projects) => {
+    let modalGallery = document.querySelector(".modal-gallery")
+    modalGallery.innerHTML = "";
+    projects.forEach((project) => {
+        const figure = document.createElement("figure");
+        const img = document.createElement("img");
+        const figcaption = document.createElement("figcaption");
+        const categoryId = document.createElement("p");
+        const deleteWork = document.createElement("i");
+
+        img.setAttribute("src", project.imageUrl);
+        figcaption.setAttribute("alt", project.title);
+        figcaption.textContent = "Editer";
+        categoryId.setAttribute("src", project.id)
+        deleteWork.classList.add("fa-solid", "fa-trash-can");
+        figure.append(img, figcaption, deleteWork);
+        modalGallery.append(figure);
+
+        deleteWork.addEventListener("click", function () {
+            fetch(urlAPI + "/" + project.id, {
+                method: "DELETE",
+                headers: {
+                    "Authorization": "Bearer " + token
+                }
+            })
+            .then(data => 
+                {
+                    deleteWork.parentNode.remove();
+                    document.querySelector(`.project-${project.id}`).remove();
+                }) 
+            .catch(error => console.error(error));
+        })
+    })
 };
 
-//Creation des different travaux
+//Fonction qui crée le DOM pour les projets filtrés
 const createDOM = (projects) => {
     const gallery = document.querySelector(".gallery");
     gallery.innerHTML = "";
-  
     projects.forEach((project) => {
-        const projectHTML = createProjectHTML(project);
-        gallery.appendChild(projectHTML);
-    });
+        const figure = document.createElement("figure");
+        const img = document.createElement("img");
+        const figcaption = document.createElement("figcaption");
+        
+
+        img.setAttribute("src", project.imageUrl);
+        img.setAttribute("alt", project.title);
+        img.setAttribute("cross-origin", "anonymous");
+    
+        figcaption.innerHTML = project.title;
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+        gallery.appendChild(figure);
+        figure.classList.add(`project-${project.id}`);
+    })
 };
 
-//Partie filtre
+//Fonction de filtres pour les projets
 const filterDataByAll = (data) => {
     return data.filter(work => work.categoryId === 1 || work.categoryId === 2 || work.categoryId === 3)
 };
@@ -50,6 +93,7 @@ const filterDataByHotel = (data) => {
     return data.filter(work => work.categoryId === 3)
 };
 
+//Fonction de filtrage pour chaque catégorie
 const filterByAll = (works) => {
     console.log(works);
     document.querySelector(".filterAll").addEventListener("click",  () => {
@@ -79,8 +123,8 @@ const filterByHotel = (works) => {
     });
 };
 
-//Partie Authentification et Modale
 
+// Si l'utilisateur est authentifié, affiche les liens pour ouvrir la modale
 if(token) {
     const modalLinks = document.querySelectorAll(".js-modal")
     document.getElementById("all-filter").style.display = "none";
@@ -88,7 +132,8 @@ if(token) {
         link.style.display = "flex";
 
         let modal = null;
-
+        
+        //Fonction pour ouvrir la modale
         const openModal = function (e) {
             e.preventDefault();
             modal = document.querySelector(e.target.getAttribute('href'));
@@ -98,11 +143,12 @@ if(token) {
             modal.querySelector('.js-modal-close').addEventListener('click', closeModal);
 
             // Afficher les projets dans la modal
-            showProjectsInModal();
+            createProjectModal(projects);
         }
 
         link.addEventListener('click', openModal);
-
+        
+        //Fonction pour fermer la modale
         const closeModal = function (e) {
             if (modal === null) return;
             if (e) {
@@ -114,32 +160,8 @@ if(token) {
         }
     });
 
-    //Mettre les projets dans la modale 
-
-    const showProjectsInModal = () => {
-        fetch(urlAPI)
-        .then((response) => response.json())
-        .then((projects) => {
-            const modalContent = document.querySelector(".modal-content");
-            modalContent.innerHTML = "";
-    
-            projects.forEach((project) => {
-                const projectHTML = createProjectHTML(project);
-                modalContent.appendChild(projectHTML);
-            });
-
-            const addProjectButton = document.createElement("button");
-            addProjectButton.innerText = "Ajouter projet";
-            addProjectButton.addEventListener("click", () => {
-                // Ajouter ici le code pour afficher le formulaire d'ajout de projet
-            });
-
-            modalContent.appendChild(addProjectButton);
-        });
-    };
-
 }else {
-    console.log("saucisson");
+    console.log("Vous n'êtes pas admin");
 }
 
 console.log(sessionStorage);
