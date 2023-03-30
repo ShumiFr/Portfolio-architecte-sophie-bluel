578374adbdc1c827b46c6e87ca456e6e479403f3
Shumi <gagnaire.pauline@gmail.com>
2023-03-30 17:05:28 +0200

    Rangement du CSS

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -2,12 +2,12 @@
 let getWorksApi = "http://localhost:5678/api/works";
 
 //Variables Travaux
-const gallery = document.querySelector(".gallery");
+const gallery = document.querySelector(".gallery-grid");
 const figures = [];
 
 //Variables Filtres
 const filters = document.querySelectorAll(".filter ul li");
-const filterAll = document.querySelector(".filterAll");
+const elementsFilter = document.querySelector(".elements-filter");
 
 //=======================================================================
 
@@ -56,7 +56,7 @@ for (let filter of filters) {
         for ( let figure of figures ) {
             if (figure.getAttribute("data-category-id") === filter.getAttribute("data-category-id")){
                 figure.style.display="block";
-            }else if ( filter === filterAll) {
+            }else if ( filter === elementsFilter) {
                 figure.style.display="block";
             }else {
                 figure.style.display="none";
